e9ede7db9998d379e9830a75536834644b487eed
Temitope Akinsoto <takinsoto@gmail.com>
2021-06-09 16:01:11 +0100

    add more css styles to Thank you page

--- src/Thanks.js
+++ src/Thanks.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Thanks() {
-    return (
-        <div style={{boxShadow: '0px 1px 3px 0px gray', marginTop: '10rem', padding: '2rem 1rem 1rem 1rem'}} >
-            <p>Your response has been successfully submitted. Thank You!</p>
-            <Link to='/'><button>Home</button></Link>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        fontSize: "16px",
+        fontWeight: "bold",
+        color: "mediumpurple",
+        fontFamily: "Verdana",
+        boxShadow: "0px 1px 3px 0px gray",
+        marginTop: "10rem",
+        padding: "2rem 1rem 1rem 1rem",
+      }}
+    >
+      <p>Your response has been successfully submitted. Thank You!</p>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+    </div>
+  );
 }
